688d402ca7d815e5f8565a88eb22681275451aaa
Cael Blackmon <cael.blackmon1@gmail.com>
2022-05-31 17:07:37 -0400

    Calculator now calculates

--- main.js
+++ main.js
@@ -6,39 +6,44 @@ const blank = document.querySelector(".blank");
 let collection = "";
 let clear = document.querySelector("#clear");
 let equals = document.querySelector("#equals");
-let sum = "";
-let result =
+
 
 console.log(clear);
 
 for (let numbers of calculator) {
   numbers.addEventListener("click", function (event) {
     console.log(event.target.textContent);
-    collection += event.target.textContent;
-    newElement.innerText = `${collection}`;
 
-    blank.appendChild(newElement);
-  });
-}
-for (let ops of operators) {
-  ops.addEventListener("click", function (event) {
-    console.log(event.target.textContent);
-    collection += event.target.textContent;
-    newElement.innerText = `${collection}`;
-
-    blank.appendChild(newElement);
-  });
-}
-
-for (numbers of calculator) {
-equals.addEventListener("click", function (event) {
-    if (number === equals) {
-    blank.innerText = eval(blank.innerText);
+    if (numbers === equals) {
+      blank.innerText = eval(blank.innerText)
     } else {
       blank.innerText += event.target.textContent
     }
-})
+
+    collection += event.target.textContent;
+    newElement.innerText = `${collection}`;
+
+  });
 }
+// for (let ops of operators) {
+//   ops.addEventListener("click", function (event) {
+//     console.log(event.target.textContent);
+//     collection += event.target.textContent;
+//     newElement.innerText = `${collection}`;
+
+//     blank.appendChild(newElement);
+//   });
+// }
+
+// for (numbers of calculator) {
+// equals.addEventListener("click", function (event) {
+//     if (number === equals) {
+//     blank.innerText = eval(blank.innerText);
+//     } else {
+//       blank.innerText += event.target.textContent
+//     }
+// })
+// }
 
 
 
